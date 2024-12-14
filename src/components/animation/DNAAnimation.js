2b5e9d837c49135122c319b872e4cdff63024813
Daniel8Valkanov8 <daniel8valkanov8@gmail.com>
2024-12-14 21:22:23 +0200

    update

--- src/components/animation/DNAAnimation.js
+++ src/components/animation/DNAAnimation.js
@@ -4,16 +4,14 @@ import "./DNAAnimation.css";
 const DNAAnimation = () => {
   const [topLetters, setTopLetters] = useState(Array(12).fill("")); // Горни букви
   const [bottomLetters, setBottomLetters] = useState(Array(12).fill("")); // Долни букви
-  const letters = ["G", "T", "A", "C"];
 
   // Функция за генериране на произволни букви
-  const generateRandomLetters = useCallback(
-    () =>
-      Array(12)
-        .fill("")
-        .map(() => letters[Math.floor(Math.random() * letters.length)]),
-    [letters]
-  );
+  const generateRandomLetters = useCallback(() => {
+    const letters = ["G", "T", "A", "C"]; // Преместено вътре в useCallback
+    return Array(12)
+      .fill("")
+      .map(() => letters[Math.floor(Math.random() * letters.length)]);
+  }, []);
 
   useEffect(() => {
     // Задаване на начални букви
